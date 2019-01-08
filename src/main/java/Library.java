a958f7f812f7390b63e44ec9802ee12401b59b85
Steven Kay <stevendkay@gmail.com>
2019-01-08 16:04:32 +0000

    more library functionality

--- src/main/java/Library.java
+++ src/main/java/Library.java
@@ -1,2 +1,19 @@
+import java.util.ArrayList;
+
 public class Library {
+
+	private ArrayList<Book> stock;
+
+	public Library() {
+		stock = new ArrayList<>();
+	}
+
+
+	public int getStockLevels() {
+		return stock.size();
+	}
+
+	public void addNewBook(Book book) {
+		stock.add(book);
+	}
 }
--- src/test/java/TestLibrary.java
+++ src/test/java/TestLibrary.java
@@ -6,9 +6,22 @@ import static org.junit.Assert.assertEquals;
 public class TestLibrary {
 
 	private Library library;
+	private Book pride;
 
 	@Before
 	public void setUp() {
 		library = new Library();
+		pride = new Book("Pride and Prejudice", "Jane Austen", "Romance");
+	}
+
+	@Test
+	public void hasBookCollection() {
+		assertEquals(0,library.getStockLevels());
+	}
+
+	@Test
+	public void canAddBook() {
+		library.addNewBook(pride);
+		assertEquals(1, library.getStockLevels());
 	}
 }
