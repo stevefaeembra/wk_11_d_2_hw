471c05e9dc3c076b9aff60a51444c7bd5f7d3c64
Steven Kay <stevendkay@gmail.com>
2019-01-08 17:15:56 +0000

    now done advanced extension

--- src/main/java/Library.java
+++ src/main/java/Library.java
@@ -1,14 +1,17 @@
 import java.lang.reflect.Array;
 import java.util.ArrayList;
+import java.util.HashMap;
 
 public class Library {
 
 	private ArrayList<Book> stock;
 	private int capacity;
+	private HashMap<String,Integer> countsByGenre;
 
 	public Library(int capacity) {
 		stock = new ArrayList<>();
 		this.capacity = capacity;
+		this.countsByGenre = new HashMap<>();
 	}
 
 	public int getStockLevels() {
@@ -22,6 +25,12 @@ public class Library {
 	public void addNewBook(Book book) {
 		if (getStockLevels() < getCapacity()) {
 			stock.add(book);
+			String genre = book.getGenre();
+			if (countsByGenre.containsKey(genre)) {
+				countsByGenre.put(genre, countsByGenre.get(genre)+1);
+			} else {
+				countsByGenre.put(genre, 1);
+			}
 		}
 	}
 
@@ -32,6 +41,16 @@ public class Library {
 	public void lendBook(Book book, Borrower customer) {
 		if (stock.remove(book)) {
 			customer.addBook(book);
+			String genre = book.getGenre();
+			countsByGenre.put(genre, countsByGenre.get(genre)-1);
+		}
+	}
+
+	public int getQuantityOfGenre(String genreName) {
+		if (countsByGenre.containsKey(genreName)) {
+			return countsByGenre.get(genreName);
+		} else {
+			return 0;
 		}
 	}
 
--- src/test/java/TestLibrary.java
+++ src/test/java/TestLibrary.java
@@ -53,4 +53,40 @@ public class TestLibrary {
 		assertEquals(catch22, customer.getLoanedBooks().get(0));
 	}
 
+	@Test
+	public void checkGenreTracking() {
+		Book book1 = new Book("book1","author1", "comedy");
+		Book book2 = new Book("book2","author2", "drama");
+		Book book3 = new Book("book3","author2", "drama");
+		Book book4 = new Book("book4","author3", "drama");
+		Book book5 = new Book("book5","author4", "romance");
+		Book book6 = new Book("book6","author5", "romance");
+		Book book7 = new Book("book7","author6", "romance");
+		Book book8 = new Book("book8","author7", "non-fiction");
+		Book book9 = new Book("book9","author7", "non-fiction");
+		Book book10 = new Book("book10","author7", "crime");
+		library.addNewBook(book1);
+		library.addNewBook(book2);
+		library.addNewBook(book3);
+		library.addNewBook(book4);
+		library.addNewBook(book5);
+		library.addNewBook(book6);
+		library.addNewBook(book7);
+		library.addNewBook(book8);
+		library.addNewBook(book9);
+		library.addNewBook(book10);
+		assertEquals(10, library.getStockLevels());
+		assertEquals(3, library.getQuantityOfGenre("drama"));
+		assertEquals(3, library.getQuantityOfGenre("romance"));
+		assertEquals(1, library.getQuantityOfGenre("comedy"));
+		assertEquals(1, library.getQuantityOfGenre("crime"));
+		assertEquals(2, library.getQuantityOfGenre("non-fiction"));
+		assertEquals(0, library.getQuantityOfGenre("monkey tennis"));
+		// now make sure it also gets updated if we loan out a book!
+		library.lendBook(book10, customer); // no crime left
+		library.lendBook(book2, customer); // down to 2 drama
+		assertEquals(0, library.getQuantityOfGenre("crime"));
+		assertEquals(2, library.getQuantityOfGenre("drama"));
+	}
+
 }
